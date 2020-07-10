e94f182f75d46bf2390b96a932bdceb32c6b35b8
Foguinel <foguinel@gmail.com>
2020-07-10 20:28:48 +0000

    making a coffee

--- index.js
+++ index.js
@@ -28,14 +28,11 @@ client.on("message", async message => {
      try {
     let commandFile = require(`./cmds/${command}.js`);
     
-    commandFile.run(client, message, args,)
+    commandFile.run(client, message, args)
     client.channels.get('517487784139685899').send(new Discord.RichEmbed().setTitle('Comando usado :sim: ').addField("Comando:", `${command}`, true).addField("Autor:", `${message.author}`, true).addField("Canal:", `${message.channel}`, true).addField("ID da Mensagem:", `${message.id}`, true).addField("Servidor:", `${message.guild.name}`, true).addField("ID do Autor:", `${message.author.id}`, true).addField("Erros na execução do comando:", `Nenhum`, true).addField("Content:", `${message.content}`, true).setColor('0x000').setThumbnail(message.author.avatarURL).setFooter("Hitter • @DeadSad_").setTimestamp())  
 } catch (err) { 
-    client.channels.get('517487784139685899').send(new Discord.RichEmbed().setTitle('Comando usado :errado: ').addField("Comando:", `${command}`, true).addField("Autor:", `${message.author}`, true).addField("Canal:", `${message.channel}`, true).addField("ID da Mensagem:", `${message.id}`, true).addField("Servidor:", `${message.guild.name}`, true).addField("ID do Autor:", `${message.author.id}`, true).addField("Erros na execução do comando:", `${err}`, true).addField("Content:", `${message.content}`, true).setColor('0x000').setThumbnail(message.author.avatarURL).setFooter("Hitter • @DeadSad_").setTimestamp())
    if(err.code != 'MODULE_NOT_FOUND'){
      console.log(err)
-  } else if(err.code == 'MODULE_NOT_FOUND'){
-    message.channel.send({embed: {description: "Não consegui encontrar o comando, ⪧ Caso precise de ajuda digite `!help`", color: 0x000}});
   }}
 
     })
