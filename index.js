775d397ead603526987b90b0197c1887d32b2f44
Foguinel <foguinel@gmail.com>
2020-07-10 20:18:59 +0000

    making a coffee

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
 
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
 client.on("ready", async => {
 	console.log("Howdy!")
 })
